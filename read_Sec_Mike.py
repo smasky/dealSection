80e400f204398e06570976cc02dc840789c882b9
smasky <492109831@qq.com>
2018-10-31 16:09:46 +0800

    add py

---
+++ read_Sec_Mike.py
@@ -0,0 +1,47 @@
+'''
+
+'''
+import os 
+from global_variable import *
+def read_section_mike(path='',name=''):
+    """
+        name: 河道名称
+        path: 文件的地址
+        从Mike读取断面信息
+    """
+    global Section
+    isPoint=False
+    isMileage=False
+    Sec_name=''
+    if(path==''):
+        print('文件地址为空')
+        return 0
+    file=open(path,'r')
+    for line in file:
+        if("LEVEL PARAMS" in line):
+            isPoint=False
+        
+        if(isMileage):
+            Sec_name=str(line).strip('\n').replace(' ','')
+            Section[Sec_name]=[]
+            isMileage=False
+        if(name in line):
+            isMileage=True
+
+        if(isPoint):
+            string=str(line).strip('\n').split('    ')
+            xy=(string[0],string[1])
+            Section[Sec_name].append(xy)
+            #print(string)
+
+        if("PROFILE" in line):
+            isPoint=True
+        
+        
+            
+#测试
+def main():
+    read_section_mike("E://aaaa.txt","HUAIHEZHUGAN1")
+
+if __name__ == '__main__':
+    main()
